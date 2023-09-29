801e732b555a7e5cf647c0279fc1abe1a7e6b3cc
Quantum Services LLP <quantumservicesllp2@Quantums-Mac-mini-2.local>
2023-09-29 16:50:55 +0530

    signup page details clear and after that data disappered

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/FirebaseConfig';
 
 function Signup() {
-    const [fname, setFname] = useState ('');
-    const [lname, setLname] = useState ('');
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -14,81 +14,84 @@ function Signup() {
         return password === confirmPassword
     }
     const signup = async () => {
-        if (fname ==="" || lname ==="" ||  email === "" || password === "" || confirmPassword === "")
-            return alert ("Please fill all fields")
-        if (password !== confirmPassword){
-            return alert ("passwords do not matched")
+        if (fname === "" || lname === "" || email === "" || password === "" || confirmPassword === "")
+            return alert("Please fill all fields")
+        if (password !== confirmPassword) {
+            return alert("passwords do not matched")
         }
         try {
-            if (arePasswordsMatching()){
-                const user = await createUserWithEmailAndPassword(auth, email, password);
-                alert("Signup Successful");
-            }
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            alert("Signup Successful, Click on Login");
+            setFname("");
+            setLname("");
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
         } catch (error) {
-            console.log(error);
-        }
+        console.log(error);
     }
-    return (
-        <div className=' flex justify-center items-center h-screen'>
-            <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
-                <div className="">
-                    <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
-                </div>
-                <div>
-                    <input type='text'
-                        value={fname}
-                        onChange={(e) => setFname(e.target.value)}
-                        name='fname'
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='First Name' 
-                    />
-                    <br/>
-                    <input type='text'
-                        value={lname}
-                        onChange={(e) => setLname(e.target.value)}
-                        name='lname'
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='Last Name' 
-                    />
-                    <br/>
-                    <input type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        name='email'
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='Email'
-                    />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword (e.target.value)}
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='Password'
-                    />
-                    <br/>
-                    <input
-                        type="text"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword (e.target.value)}
-                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='Confirm Password'
-                    />
-                </div>
-                <div className=' flex justify-center mb-3'>
-                    <button
-                        onClick={signup}
-                        className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
-                        Signup
-                    </button>
-                </div>
-                <div>
-                    <h2 className='text-white'>Don't have an account <Link className=' text-red-500 font-bold' to={'/login'}>Login</Link></h2>
-                </div>
+}
+return (
+    <div className=' flex justify-center items-center h-screen'>
+        <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
+            <div className="">
+                <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
+            </div>
+            <div>
+                <input type='text'
+                    value={fname}
+                    onChange={(e) => setFname(e.target.value)}
+                    name='fname'
+                    className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                    placeholder='First Name'
+                />
+                <br />
+                <input type='text'
+                    value={lname}
+                    onChange={(e) => setLname(e.target.value)}
+                    name='lname'
+                    className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                    placeholder='Last Name'
+                />
+                <br />
+                <input type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name='email'
+                    className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                    placeholder='Email'
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                    placeholder='Password'
+                />
+                <br />
+                <input
+                    type="text"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                    placeholder='Confirm Password'
+                />
+            </div>
+            <div className=' flex justify-center mb-3'>
+                <button
+                    onClick={signup}
+                    className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
+                    Signup
+                </button>
+            </div>
+            <div>
+                <h2 className='text-white'>Already have an account <Link className=' text-red-500 font-bold' to={'/login'}>Login</Link></h2>
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default Signup;
